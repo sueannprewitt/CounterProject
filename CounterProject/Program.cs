6a4b2074b4714e1352f40e71c1443c1ad6de96f8
Sue Ann <sueannprewitt@yahoo.com>
2017-09-07 11:43:53 -0400

    Initial commit

--- CounterProject/Program.cs
+++ CounterProject/Program.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace CounterProject {
 	class Program {
+		int factorial (int number) {
+			var total = 1;
+			for (var idx = 2; idx <= number; idx++)	{
+				total = total * idx;
+			}
+			return total;
+		}
+		void Run() {
+			Console.WriteLine($"The Factorial of 5 is {factorial(5)}");
+		}
+
+		
 		static void Main(string[] args) {
+			new Program().Run();
 		}
 	}
 }
